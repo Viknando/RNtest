64c1e914c6e4621b0ca2b0f382610fa52d9768e1
xiecaiji <xie_caiji@want-want.com>
2018-03-02 16:44:12 +0800

    rn页面->rn页面的跳转

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import pageOne from './pageOne'
 
-AppRegistry.registerComponent('RNjsWithNative', () => App);
+
+//页面跳转功能实现需要 StackNavigator,如果没有react-navigation需要 yarn add react-navigation 安装react-navigation
+import {
+    StackNavigator,
+} from 'react-navigation';
+const app = StackNavigator({
+    App: {screen: App},
+    pageOne: {screen: pageOne},
+});
+
+
+AppRegistry.registerComponent('RNjsWithNative', () => app);
--- pageOne.js
+++ pageOne.js
@@ -12,27 +12,12 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class pageOne extends Component<> {
   render() {
-    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome} onPress={() => navigate('pageOne')}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+        <Text style={styles.welcome} onPress={() => navigate('App')}>
+          Back to App!
         </Text>
       </View>
     );
